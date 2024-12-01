ee327656028d6e96d3ff0146512ec679e6ffda3e
Yeon Lee <yeonholee50@gmail.com>
2024-12-01 15:47:28 -0500

    supports wifi sniffing basics - won't be implementing android for some tiem -first windows and linux and macos

--- wifi_info.c
+++ wifi_info.c
@@ -6,8 +6,19 @@
 void get_wifi_info() {
     #ifdef __linux__
         // Linux: Using nmcli to get Wi-Fi information
-        printf("Fetching Wi-Fi networks on Linux...\n");
+        printf("Fetching Wi-Fi networks on Ubuntu/Linux...\n");
         system("nmcli -t -f active,ssid,signal,frequency dev wifi");
+
+    #elif __APPLE__
+        // macOS: Using system_profiler or airport for Wi-Fi information
+        printf("Fetching Wi-Fi networks on macOS...\n");
+        system("system_profiler SPAirPortDataType");
+
+    #elif __ANDROID__
+        // Android: This part requires native Android development (Java/Kotlin)
+        // You cannot fetch Wi-Fi info directly in a C program on Android
+        printf("Android: Wi-Fi information cannot be fetched using C in this context.\n");
+        // To get Wi-Fi information on Android, you would need to use Java/Kotlin with WifiManager API.
 
     #elif _WIN32
         // Windows: Using netsh to get Wi-Fi information
@@ -24,3 +35,4 @@ int main() {
     get_wifi_info();
     return 0;
 }
+
